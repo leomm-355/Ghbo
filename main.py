ebd72ec4dc1e0d94d443de0438e90a85068a5526
leomm-355 <leomax24407@gmail.com>
2026-08-18 11:25:32 +0630

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+#999
+
+import logging
+from pyrogram import Client
+import config
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+
+app = Client(
+    "ManagementBot",
+    api_id=config.API_ID,
+    api_hash=config.API_HASH,
+    bot_token=config.BOT_TOKEN,
+    plugins=dict(root="tg_bot.modules") 
+
+if __name__ == "__main__":
+    logger.info("🤖 Bot စတင်အလုပ်လုပ်နေပါပြီ...")
+    app.run()
