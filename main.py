c09ca0b983ce5a35f03ef97e76532409b6f38dc6
leomm-355 <leomax24407@gmail.com>
2026-08-18 11:32:38 +0630

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = Client(
     api_id=config.API_ID,
     api_hash=config.API_HASH,
     bot_token=config.BOT_TOKEN,
-    plugins=dict(root="tg_bot.modules") 
+    plugins=dict(root="tg_bot") 
 
 if __name__ == "__main__":
     logger.info("🤖 Bot စတင်အလုပ်လုပ်နေပါပြီ...")
